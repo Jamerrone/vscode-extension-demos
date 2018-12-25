7ea5a17e6eab059c7dbf8c04d4278cd5124c5b85
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2018-12-25 13:19:11 +0100

    Demo 1: Finish Getting Started.

--- demo-1-hello-world/extension.js
+++ demo-1-hello-world/extension.js
@@ -18,17 +18,23 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'extension.helloWorld',
+  let helloVSCode = vscode.commands.registerCommand(
+    'extension.helloVSCode',
     function() {
       // The code you place here will be executed every time your command is executed
 
       // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World!');
+      vscode.window.showInformationMessage('Hello VS Code!');
     },
   );
 
-  context.subscriptions.push(disposable);
+  let currentTime = vscode.commands.registerCommand(
+    'extension.currentTime',
+    () => vscode.window.showInformationMessage(new Date().toLocaleTimeString()),
+  );
+
+  context.subscriptions.push(helloVSCode);
+  context.subscriptions.push(currentTime);
 }
 exports.activate = activate;
 
